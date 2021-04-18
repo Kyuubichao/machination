a61166e0aa28ff6d05fb78128596707c491bde09
Kenneth <treyneads@outlook.com>
2021-04-18 16:15:03 -0400

    added vuetify and fixed linting

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,90 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app>
+    <v-app-bar app clipped-left fixed>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <router-link to="/" tag="h3" class="pointer ml-5"> Machination </router-link>
+      <v-spacer />
+      <span v-if="user" class="mr-5">{{ userDisplayName }}</span>
+      <v-btn v-if="!user" outlined class="mr-5" @click="nav('login')"> Login </v-btn>
+      <v-btn v-if="!user" outlined @click="nav('register')"> Register </v-btn>
+      <v-btn v-if="user" @click="logout" outlined> Logout </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <v-navigation-drawer v-model="drawer" app clipped disable-resize-watcher>
+        <v-list nav>
+          <v-list-item @click="nav('about')" style="justify-content: start">
+            <v-icon class="mr-5"> mdi-information </v-icon>
+            <span>About</span>
+          </v-list-item>
+          <v-list-item @click="nav('lights')">
+            <v-icon class="mr-5"> mdi-gamepad-variant-outline </v-icon>
+            <span>Lights Out</span>
+          </v-list-item>
+          <v-list-item>
+            <v-icon class="mr-5" />
+          </v-list-item>
+          <v-list-item>
+            <v-icon class="mr-5" />
+          </v-list-item>
+          <v-list-item>
+            <v-icon class="mr-5" />
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+      <v-container fluid fill-height justify-center @click.stop="drawer = false">
+        <router-view @click.stop="drawer = false" />
+      </v-container>
+    </v-content>
+
+    <v-footer app>
+      <span>&copy; 2020 Version 0.1.0 </span>
+    </v-footer>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import { mapState } from 'vuex';
+export default {
+  name: 'App',
+  props: {},
 
-#nav {
-  padding: 30px;
-}
+  created() {
+    this.$vuetify.theme.dark = true;
+  },
+  computed: {
+    userDisplayName() {
+      if (this.user) {
+        return this.users.data.firstName + ' ' + this.users.data.lastName;
+      }
+      return '';
+    },
+    ...mapState(['user', 'users']),
+  },
+  data() {
+    return {
+      drawer: true,
+    };
+  },
+  methods: {
+    navigateHome() {
+      this.$router.push({ name: 'Home' });
+    },
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+    logout() {
+      this.$store.commit('setUser', null);
+      this.$store.dispatch('users/closeDBChannel', { clearModule: true });
+      this.$firebase.auth().signOut();
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+      console.log(this.$firebase);
+
+      // this.$firebase.auth().signOut().then(resp => { console.log('Logout Successful', resp) }).catch(err => { console.log(err) })
+    },
+  },
+};
+</script>
+<style scoped>
+.pointer {
+  cursor: pointer;
 }
 </style>
